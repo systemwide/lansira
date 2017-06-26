d50f97c4ee755782fae2adcc47164fe707c4899d
Ben Rotolo <benrotolo@Bens-MacBook-Pro.local>
2017-06-26 16:28:03 -0400

    first actual commit

---
+++ background.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+document.getElementById('button1').addEventListener("click", tabMaker)
+
+function tabMaker() {
+	var initialTab = "http://www.uga.edu";
+	chrome.tabs.create({ "url" : "initialTab" })
+};
+}
